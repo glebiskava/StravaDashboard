76e334d9614772e2d322ca7bfb170ff5616f7dfd
glebiskava <leopold.mittelberger@gmail.com>
2025-04-18 11:13:04 +0200

    - feature: added nav bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
 import { ActivityListComponent } from './components/activity-list/activity-list.component';
 import { WeeklySummaryComponent } from './components/weekly-summary/weekly-summary.component';
 import { MonthlySummaryComponent } from './monthly-summary/monthly-summary.component';
@@ -9,6 +20,23 @@ import { MonthlySummaryComponent } from './monthly-summary/monthly-summary.compo
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, ActivityListComponent, WeeklySummaryComponent, MonthlySummaryComponent]
+  imports: [
+    CommonModule,
+    ActivityListComponent,
+    WeeklySummaryComponent,
+    MonthlySummaryComponent,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenav,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  isSidenavOpen = false;
+}
